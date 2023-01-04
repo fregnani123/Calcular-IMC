11a50ece899de903b6bb5a914a0727d2a72499f9
fregnani123 <fabianofregnani@hotmail.com>
2023-01-04 19:59:12 -0300

    Atualizar

--- js/script.js
+++ js/script.js
@@ -1,18 +1,68 @@
 
 
-const nome = "Luiz Otario";
-const  sobrenome = "Miranda";
-const idade = 30;
-const peso= 84;
-const altura = 1.80;
+ function meuEscopo () {
+   const form = document.querySelector(".form");
+   const res = document.querySelector("#res");
+    
+   const pessoas = []
 
-let imc = Math.floor(peso / (altura * altura));
+   function recebeEventoForm (evento){
+     evento.preventDefault();
+    
+     const nome = form.querySelector(".nome").value;
+     const sobrenome = form.querySelector(".sobrenome").value;
+     const peso = form.querySelector(".peso").value;
+     const altura = form.querySelector(".altura").value;
+     
+    pessoas.push({nome,sobrenome,peso,altura})
+      
+    console.log(pessoas)
+   }
 
-let anoNasmento = 2023 - idade;
+ form.addEventListener("submit", recebeEventoForm );
 
- `${nome} ${sobrenome}, nascido no ano de ${anoNasmento} com a idade de ${idade} anos tem a taxa de IMC de ${imc}`
+}
+
+meuEscopo()
 
 
- let p = document.createElement("p")
- let div = document.querySelector("#div").appendChild(p).innerHTML = `${nome} ${sobrenome}, nascido no ano de ${anoNasmento} com a idade de ${idade} anos tem a taxa de IMC de ${imc}`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // let n = 10
+// let imprimir = document.querySelector("#div")
+
+
+// let pessoas = [{nome:"Fernada"},{nome:"Fabiano"},{nome:"Fabricio"},{nome:"Ada"},{nome:"Fran"},{nome:"Bicudo"},{nome:"Maurina"},{nome:"Alline"},{nome:"jane"}]
+
+// pessoas.push({nome:"Giovani"})
+
+// console.log(pessoas)
+
+// function sortear(){
+//     let sorte = Math.round(Math.random() *  n);
+
+//   if( sorte === 5){
+//     imprimir.innerHTML = "Você é sortudo! numero 5"
+//   }else{imprimir.innerHTML = sorte } 
+// }
+
 
